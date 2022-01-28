4e5261cae1cea7a39ebfb62022859ef500f97276
fleevo32 <98611984+fleevo32@users.noreply.github.com>
2022-01-28 18:15:31 -0500

    Fixed export for new notification system

--- resources/[standalone]/orp_banking/config.lua
+++ resources/[standalone]/orp_banking/config.lua
@@ -26,7 +26,7 @@ SendNotify = function(msg, type)
 	--	text = msg,
 	--	length = 7500
 	--})
-	exports['notify1']:Alert("Information", msg, 2500, 'info')                              -- https://github.com/ChinoRD/dopeNotifyV2
+	exports['dopeNotifyV2']:Alert("Information", msg, 2500, 'info')                              -- https://github.com/ChinoRD/dopeNotifyV2
 	-- TriggerEvent('esx:showNotification', msg)						-- Default GTA V notification.
 	-- TriggerEvent('t-notify:client:Alert', { style = type, message = msg })		-- https://github.com/TasoOneAsia/t-notify
 	-- TriggerEvent('b1g_notify:client:Notify', { type = type, text = msg })		-- https://github.com/CarlosVergikosk/B1G_NOTIFY
